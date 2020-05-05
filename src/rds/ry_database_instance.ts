194f41f006ca10c38d51b1ab657134b1e0f34ef7
Calvin Yu <me@sourcebender.com>
2020-05-05 02:01:45 -0400

    Create lookup for postgres instance

--- src/rds/ry_database_instance.ts
+++ src/rds/ry_database_instance.ts
@@ -1,7 +1,12 @@
-import { DatabaseInstance, DatabaseInstanceProps } from "@aws-cdk/aws-rds";
+import {
+  DatabaseInstance,
+  DatabaseInstanceProps,
+  IDatabaseInstance,
+} from "@aws-cdk/aws-rds";
 import { SecretValue, Construct, Duration } from "@aws-cdk/core";
 import {
   SecurityGroup,
+  ISecurityGroup,
   InstanceType,
   InstanceClass,
   InstanceSize,
@@ -24,31 +29,64 @@ export type BaseRyDatabaseInstanceProps = Omit<
   "engine"
 >;
 
+export interface IRyDatabaseInstance extends IDatabaseInstance {
+  masterUsername: string;
+  masterPassword: SecretValue;
+  databaseName: string;
+}
+
+export interface RyDatabaseInstanceAttributes {
+  readonly conventions?: Conventions;
+  readonly masterUsername?: string;
+  readonly masterPassword: SecretValue;
+  readonly databaseName?: string;
+  readonly instanceIdentifier?: string;
+  readonly instanceEndpointAddress: string;
+  readonly port: number;
+  readonly securityGroups?: ISecurityGroup[];
+}
+
 export interface RyDatabaseInstanceProps extends AltDatabaseInstanceProps {
   readonly conventions: Conventions;
   readonly masterUsername?: string;
   readonly instanceClass?: InstanceType;
 }
 
-export default class RyDatabaseInstance extends DatabaseInstance {
+function defaultMasterUsername(conventions: Conventions) {
+  return `${conventions.eqn("camel")}DbUser`;
+}
+
+function defaultDatabaseName(conventions: Conventions) {
+  return conventions.eqn("underscore");
+}
+
+function defaultInstanceIdentifier(conventions: Conventions) {
+  return conventions.eqn();
+}
+
+export default class RyDatabaseInstance extends DatabaseInstance
+  implements IRyDatabaseInstance {
   masterUsername: string;
   masterPassword: SecretValue;
   databaseName: string;
   securityGroup: SecurityGroup;
 
   constructor(scope: Construct, id: string, props: RyDatabaseInstanceProps) {
+    const { conventions } = props;
     const {
-      conventions,
-
-      masterUsername = `${conventions.eqn("camel")}DbUser`,
+      masterUsername = defaultMasterUsername(conventions),
+      databaseName = defaultDatabaseName(conventions),
+      instanceIdentifier = defaultInstanceIdentifier(conventions),
+    } = props;
+    const {
       masterUserPassword = new Secret(
         scope,
 
         // this name appears to get truncated by Secrets Manager, so best to best
         // to keep it short.
         `${conventions.eqn("path")}dbMasterPassword`,
-
         {
+          description: `password for ${conventions.eqn()} database`,
           generateSecretString: {
             includeSpace: false,
 
@@ -59,15 +97,12 @@ export default class RyDatabaseInstance extends DatabaseInstance {
 
             passwordLength: 16,
             secretStringTemplate: JSON.stringify({
-              username: `${conventions.eqn("camel")}DbUser`,
+              username: masterUsername,
             }),
             generateStringKey: "password",
           },
         }
       ).secretValueFromJson("password"),
-
-      databaseName = conventions.eqn("underscore"),
-      instanceIdentifier = conventions.eqn(),
     } = props;
 
     super(scope, id, {
@@ -83,5 +118,49 @@ export default class RyDatabaseInstance extends DatabaseInstance {
     this.masterUsername = masterUsername;
     this.masterPassword = masterUserPassword;
     this.databaseName = databaseName;
+  }
+
+  static fromRyDatabaseInstanceAttributes(
+    scope: Construct,
+    id: string,
+    attrs: RyDatabaseInstanceAttributes
+  ): IRyDatabaseInstance {
+    const { conventions } = attrs;
+    let {
+      masterPassword,
+      masterUsername,
+      databaseName,
+      instanceIdentifier,
+      instanceEndpointAddress,
+      port,
+      securityGroups = [],
+    } = attrs;
+
+    if (conventions) {
+      if (!masterUsername) masterUsername = defaultMasterUsername(conventions);
+      if (!databaseName) {
+        databaseName = defaultDatabaseName(conventions);
+      }
+      if (!instanceIdentifier) {
+        instanceIdentifier = defaultInstanceIdentifier(conventions);
+      }
+    } else if (!masterUsername || !databaseName || !instanceIdentifier) {
+      throw new Error(
+        "masterUsername, databaseName, instanceIdentifier attributes are required if conventions is not specified"
+      );
+    }
+
+    const instance = DatabaseInstance.fromDatabaseInstanceAttributes(
+      scope,
+      `id,
+      {
+        instanceIdentifier,
+        instanceEndpointAddress,
+        port,
+        securityGroups,
+      }
+    );
+
+    return { ...instance, masterUsername, masterPassword, databaseName };
   }
 }
