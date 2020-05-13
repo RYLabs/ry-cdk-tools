efef6a243a40798b9eeb3482038938d33d88f686
scope2229 <kaine_76@hotmail.com>
2020-05-13 22:36:22 +0800

    changed how cloudfront is created to dry up the code base

--- src/spa/index.ts
+++ src/spa/index.ts
@@ -2,5 +2,5 @@ export { default as S3SpaPipelineStack } from "../stacks/s3_spa_pipeline_stack";
 export * from "../stacks/s3_spa_pipeline_stack";
 export { default as S3Spa } from "../spa/s3_spa";
 export * from "../spa/s3_spa";
-export { default as CloudfrontSpa } from "../spa/cloudfront_spa";
-export * from "../spa/cloudfront_spa";
+export { default as CloudFrontConfig } from "./cloudfront_config";
+export * from "./cloudfront_config";
