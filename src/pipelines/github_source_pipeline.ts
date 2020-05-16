1afbcbab965c8eebf3fb555afa0ee7b8a54852d8
scope2229 <kaine_76@hotmail.com>
2020-05-17 03:32:51 +0800

    updated github_source_pipeline

--- src/pipelines/github_source_pipeline.ts
+++ src/pipelines/github_source_pipeline.ts
@@ -2,38 +2,40 @@ import { Construct, SecretValue, RemovalPolicy } from "@aws-cdk/core";
 import { Pipeline, Artifact } from "@aws-cdk/aws-codepipeline";
 import { Alias, Key } from "@aws-cdk/aws-kms";
 import { BucketEncryption, BlockPublicAccess, Bucket } from "@aws-cdk/aws-s3";
+import { Secret } from '@aws-cdk/aws-secretsmanager';
 import {
   GitHubSourceAction,
   GitHubTrigger,
 } from "@aws-cdk/aws-codepipeline-actions";
 import _ = require("lodash");
 
-/**
- * Create a Keys type that declares its type
- */
-type Keys<T> = keyof T;
-function Keys<T>(o: T) {
-  if (!o) {
-    return [];
-  }
-  return Object.keys(o) as Keys<T>[];
-}
-/**
- * Refractored code to make OneOf less smelly
- */
-type EachOfTmp<T> = {
-  [K in Keys<T>]: {
-    _: { [X in K]: T[K] };
-  };
-};
-/**
- * When parameters are confitional but one must be used
- * This type should be used.
- * const thing1: OneOf<{ a: number; b: number }> = { a: 2 } // valid
- * const thing2: OneOf<{ a: number; b: number }> = { b: 2 } // valid
- * const thing3: OneOf<{ a: number; b: number }> = {} // invalid
- */
-type OneOf<T> = EachOfTmp<T>[Keys<T>]["_"] & Partial<T>;
+// /**
+//  * Create a Keys type that declares its type
+//  */
+// type Keys<T> = keyof T;
+// function Keys<T>(o: T) {
+//   if (!o) {
+//     return [];
+//   }
+//   return Object.keys(o) as Keys<T>[];
+// }
+// /**
+//  * Refractored code to make OneOf less smelly
+//  */
+// type EachOfTmp<T> = {
+//   [K in Keys<T>]: {
+//     _: { [X in K]: T[K] };
+//   };
+// };
+// /**
+//  * When parameters are confitional but one must be used
+//  * This type should be used.
+//  * const thing1: OneOf<{ a: number; b: number }> = { a: 2 } // valid
+//  * const thing2: OneOf<{ a: number; b: number }> = { b: 2 } // valid
+//  * const thing3: OneOf<{ a: number; b: number }> = {} // invalid
+//  */
+// type OneOf<T> = EachOfTmp<T>[Keys<T>]["_"] & Partial<T>;
+type KeySource = string | SecretValue;
 
 export interface GithubSourcePipelineProps {
   ownerName: string;
@@ -47,7 +49,8 @@ export interface GithubSourcePipelineProps {
    * or pass a string and have the secretsmanager
    * search for the key
    */
-  githubAuth: OneOf<{ githubOAuthToken: SecretValue; secretKey: string }>;
+  githubOAuthToken: KeySource;
+  // githubAuth: OneOf<{ githubOAuthToken: SecretValue; secretKey: string }>;
   bucketName: string;
 }
 
@@ -113,13 +116,14 @@ export default class GithubSourcePipeline extends Construct {
    * Check if the user provided a string or SecretValue
    * @param source
    */
-  private checkTypeOfAuth(source: any) {
-    if (_.isString(source)) {
-      return SecretValue.secretsManager(source, { jsonField: source });
-    } else {
+  private checkTypeOfAuth(source: KeySource) {
+    if (source instanceof SecretValue) {
       return source;
+    } else {
+      return SecretValue.secretsManager(source, { jsonField: source });
     }
-  }
+  };
+  
   constructor(scope: Construct, id: string, props: GithubSourcePipelineProps) {
     super(scope, id);
 
@@ -127,7 +131,8 @@ export default class GithubSourcePipeline extends Construct {
       repoName,
       ownerName,
       branchName = "master",
-      githubAuth,
+      githubOAuthToken,
+      // githubAuth,
       bucketName,
     } = props;
 
@@ -144,7 +149,8 @@ export default class GithubSourcePipeline extends Construct {
               repo: repoName,
               owner: ownerName,
               branch: branchName,
-              oauthToken: this.checkTypeOfAuth(githubAuth),
+              // oauthToken: this.checkTypeOfAuth(githubAuth),
+              oauthToken: this.checkTypeOfAuth(githubOAuthToken),
               output: githubRepoOutput,
               trigger: GitHubTrigger.WEBHOOK,
             }),
