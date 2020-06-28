81c36add8eddcd1deb21c2ac3037f28d3bdda776
Calvin Yu <me@sourcebender.com>
2020-06-28 16:08:52 -0400

    v0.0.17

--- src/base_stacks/base_stack.ts
+++ src/base_stacks/base_stack.ts
@@ -3,7 +3,7 @@ import Conventions from "../utils/conventions";
 import { AppInfo } from "../utils/app_info";
 
 export interface BaseStackProps extends StackProps {
-  appInfo: AppInfo
+  appInfo: AppInfo;
 }
 
 export default class BaseStack extends Stack {
@@ -11,9 +11,9 @@ export default class BaseStack extends Stack {
 
   constructor(scope: Construct, id: string, props: BaseStackProps) {
     super(scope, id, props);
-    
+
     const { appInfo } = props;
-    
+
     const conventions = new Conventions(appInfo);
     conventions.tag(this);
     this.conventions = conventions;
